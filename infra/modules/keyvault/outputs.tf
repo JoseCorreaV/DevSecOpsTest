84205117af42faa8defdf9c46c5111687a48e31e
Jose Stiven Correa Valoyes <103680054+JoseCorreaV@users.noreply.github.com>
2026-02-11 14:53:03 -0500

    Update outputs.tf

--- infra/modules/keyvault/outputs.tf
+++ infra/modules/keyvault/outputs.tf
@@ -6,11 +6,7 @@ output "vault_uri" {
   value = azurerm_key_vault.this.vault_uri
 }
 
-output "keyvault_name" {
-  value = azurerm_key_vault.this.name
-}
-
-output "secret_id" {
-  # versionless_id sirve para KeyVaultUrl en Container Apps/Jobs
-  value = azurerm_key_vault_secret.my_secret.versionless_id
+# URL versionless del secreto (útil para Container Apps / Jobs con KeyVaultUrl)
+output "secret_versionless_id" {
+  value = "${azurerm_key_vault.this.vault_uri}secrets/${azurerm_key_vault_secret.my_secret.name}"
 }
