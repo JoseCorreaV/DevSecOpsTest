a4d1b3a21764c120838700a9a1e91a0e78208b26
Jose Stiven Correa Valoyes <103680054+JoseCorreaV@users.noreply.github.com>
2026-02-11 11:40:17 -0500

    Update outputs.tf

--- infra/envs/dev/outputs.tf
+++ infra/envs/dev/outputs.tf
@@ -10,6 +10,14 @@ output "keyvault_id" {
   value = module.keyvault.key_vault_id
 }
 
+output "keyvault_name" {
+  value = module.keyvault.key_vault_name
+}
+
+output "vault_uri" {
+  value = module.keyvault.vault_uri
+}
+
 output "containerapp_name" {
   value = "${var.prefix}-api"
 }
@@ -20,4 +28,12 @@ output "job_name" {
 
 output "uami_id" {
   value = module.identity.id
+}
+
+output "uami_principal_id" {
+  value = module.identity.principal_id
+}
+
+output "cae_environment_id" {
+  value = module.cae.environment_id
 }
