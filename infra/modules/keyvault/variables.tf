59e982e50bb080a14a188d14e0492f335788bff8
Jose Stiven Correa Valoyes <103680054+JoseCorreaV@users.noreply.github.com>
2026-02-11 13:59:38 -0500

    Update variables.tf

--- infra/modules/keyvault/variables.tf
+++ infra/modules/keyvault/variables.tf
@@ -1,13 +1,25 @@
-variable "resource_group_name" { type = string }
-variable "location"            { type = string }
-variable "prefix"              { type = string }
+variable "prefix" {
+  type        = string
+  description = "Prefijo para naming. Ej: techflowdev"
+}
 
-variable "secrets_officer_principal_ids" {
-  type        = list(string)
-  description = "Azure AD object IDs con permisos 'Key Vault Secrets Officer' sobre el vault."
+variable "location" {
+  type        = string
+  description = "Azure region. Ej: eastus"
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "Resource group donde se crea el KeyVault."
 }
 
 variable "my_secret_value" {
-  type      = string
-  sensitive = true
+  type        = string
+  description = "Valor del secreto my-secret."
+  sensitive   = true
+}
+
+variable "secrets_officer_principal_ids" {
+  type        = list(string)
+  description = "Azure AD Object IDs con rol 'Key Vault Secrets Officer' sobre el vault."
 }
