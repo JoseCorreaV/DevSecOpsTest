3a5d807720b1bb591b24b420a10abb32ceff5687
Jose Stiven Correa Valoyes <103680054+JoseCorreaV@users.noreply.github.com>
2026-02-11 19:47:07 -0500

    Update main.tf

--- infra/modules/containerapp_job/main.tf
+++ infra/modules/containerapp_job/main.tf
@@ -10,7 +10,7 @@ resource "azurerm_role_assignment" "acr_pull" {
 
   name = uuidv5(
     "6ba7b811-9dad-11d1-80b4-00c04fd430c8",
-    "${var.acr_id}|AcrPull|${var.identity_principal_id}"
+    "${var.acr_id}|AcrPull|job|${var.identity_principal_id}"
   )
 }
 
@@ -21,75 +21,29 @@ resource "azurerm_role_assignment" "kv_secrets_user" {
 
   name = uuidv5(
     "6ba7b811-9dad-11d1-80b4-00c04fd430c8",
-    "${var.keyvault_id}|Key Vault Secrets User|${var.identity_principal_id}"
+    "${var.keyvault_id}|Key Vault Secrets User|job|${var.identity_principal_id}"
   )
-
-  skip_service_principal_aad_check = true
 }
 
-resource "azapi_resource" "job" {
-  type      = "Microsoft.App/jobs@2023-05-01"
-  name      = "${var.prefix}job"
-  location  = var.location
-  parent_id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${var.resource_group_name}"
-
-  schema_validation_enabled = false
+resource "azurerm_container_app_job" "this" {
+  name                         = "${var.prefix}-job"
+  location                     = var.location
+  resource_group_name          = var.resource_group_name
+  container_app_environment_id = var.cae_environment_id
 
   identity {
     type         = "UserAssigned"
     identity_ids = [var.identity_id]
   }
 
-  body = {
-    properties = {
-      environmentId = var.environment_id
+  trigger_type = "Manual"
 
-      configuration = {
-        triggerType       = "Manual"
-        replicaRetryLimit = 0
-        replicaTimeout    = 300
-
-        registries = [
-          {
-            server   = var.acr_login_server
-            identity = var.identity_id
-          }
-        ]
-
-        secrets = [
-          {
-            name        = "my-secret"
-            keyVaultUrl = var.keyvault_secret_id
-            identity    = var.identity_id
-          }
-        ]
-      }
-
-      template = {
-        containers = [
-          {
-            name  = "job"
-            image = "${var.acr_login_server}/${var.job_image_name}:${var.job_image_tag}"
-
-            env = [
-              {
-                name      = "MY_SECRET"
-                secretRef = "my-secret"
-              }
-            ]
-
-            resources = {
-              cpu    = 0.25
-              memory = "0.5Gi"
-            }
-          }
-        ]
-      }
+  template {
+    container {
+      name   = "job"
+      image  = "${var.acr_login_server}/${var.job_image_name}:${var.job_image_tag}"
+      cpu    = 0.25
+      memory = "0.5Gi"
     }
   }
-
-  depends_on = [
-    azurerm_role_assignment.acr_pull,
-    azurerm_role_assignment.kv_secrets_user
-  ]
 }
