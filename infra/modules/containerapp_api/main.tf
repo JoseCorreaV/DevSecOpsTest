2c9dc9c6fc3010ce8f62b7ba24b323c22762898d
Jose Stiven Correa Valoyes <103680054+JoseCorreaV@users.noreply.github.com>
2026-02-11 19:46:34 -0500

    Update main.tf

--- infra/modules/containerapp_api/main.tf
+++ infra/modules/containerapp_api/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_role_assignment" "acr_pull" {
 
   name = uuidv5(
     "6ba7b811-9dad-11d1-80b4-00c04fd430c8",
-    "${var.acr_id}|AcrPull|${var.identity_principal_id}"
+    "${var.acr_id}|AcrPull|api|${var.identity_principal_id}"
   )
 }
 
@@ -18,94 +18,54 @@ resource "azurerm_role_assignment" "kv_secrets_user" {
 
   name = uuidv5(
     "6ba7b811-9dad-11d1-80b4-00c04fd430c8",
-    "${var.keyvault_id}|Key Vault Secrets User|${var.identity_principal_id}"
+    "${var.keyvault_id}|Key Vault Secrets User|api|${var.identity_principal_id}"
   )
-
-  skip_service_principal_aad_check = true
 }
 
-resource "azapi_resource" "api" {
-  type      = "Microsoft.App/containerApps@2023-05-01"
-  name      = "${var.prefix}api"
-  location  = var.location
-  parent_id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${var.resource_group_name}"
-
-  ignore_missing_property   = true
-  schema_validation_enabled = false
+resource "azurerm_container_app" "this" {
+  name                         = "${var.prefix}-api"
+  container_app_environment_id = var.cae_environment_id
+  resource_group_name          = var.resource_group_name
+  revision_mode                = "Single"
 
   identity {
     type         = "UserAssigned"
     identity_ids = [var.identity_id]
   }
 
-  body = {
-    properties = {
-      managedEnvironmentId = var.environment_id
-
-      configuration = {
-        ingress = {
-          external   = true
-          targetPort = 8080
-          traffic = [
-            { latestRevision = true, weight = 100 }
-          ]
-        }
-
-        registries = [
-          {
-            server   = var.acr_login_server
-            identity = var.identity_id
-          }
-        ]
-
-        secrets = [
-          {
-            name        = "my-secret"
-            keyVaultUrl = var.keyvault_secret_id
-            identity    = var.identity_id
-          }
-        ]
-      }
-
-      template = {
-        # INIT CONTAINER (cumple el requisito del doc)
-        initContainers = [
-          {
-            name    = "init"
-            image   = "alpine:3.20"
-            command = ["/bin/sh"]
-            args    = ["-lc", "echo 'Iniciando...' && sleep 5"]
-            resources = {
-              cpu    = 0.1
-              memory = "0.1Gi"
-            }
-          }
-        ]
-
-        containers = [
-          {
-            name  = "api"
-            image = "${var.acr_login_server}/${var.app_image_name}:${var.app_image_tag}"
-
-            env = [
-              {
-                name      = "MY_SECRET"
-                secretRef = "my-secret"
-              }
-            ]
-
-            resources = {
-              cpu    = 0.25
-              memory = "0.5Gi"
-            }
-          }
-        ]
-      }
+  ingress {
+    external_enabled = true
+    target_port      = 3000
+    traffic_weight {
+      latest_revision = true
+      percentage      = 100
     }
   }
 
-  depends_on = [
-    azurerm_role_assignment.acr_pull,
-    azurerm_role_assignment.kv_secrets_user
-  ]
+  template {
+    min_replicas = 1
+    max_replicas = 1
+
+    init_container {
+      name  = "init"
+      image = "alpine:3.20"
+      command = [
+        "sh",
+        "-lc",
+        "echo \"Iniciando...\""
+      ]
+    }
+
+    container {
+      name   = "api"
+      image  = "${var.acr_login_server}/${var.app_image_name}:${var.app_image_tag}"
+      cpu    = 0.25
+      memory = "0.5Gi"
+
+      env {
+        name  = "MY_SECRET"
+        value = var.my_secret_value
+      }
+    }
+  }
 }
