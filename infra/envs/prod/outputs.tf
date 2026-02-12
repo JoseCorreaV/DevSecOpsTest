891775bd989771cd4d00ec541902212888c415de
Jose Stiven Correa Valoyes <103680054+JoseCorreaV@users.noreply.github.com>
2026-02-12 12:55:19 -0500

    Update outputs.tf

--- infra/envs/prod/outputs.tf
+++ infra/envs/prod/outputs.tf
@@ -10,20 +10,12 @@ output "cae_environment_id" {
   value = module.cae.environment_id
 }
 
-output "uami_id" {
-  value = module.identity.id
-}
-
-output "uami_principal_id" {
-  value = module.identity.principal_id
-}
-
 output "keyvault_id" {
   value = module.keyvault.key_vault_id
 }
 
 output "keyvault_name" {
-  value = module.keyvault.name
+  value = module.keyvault.key_vault_name
 }
 
 output "vault_uri" {
@@ -32,4 +24,12 @@ output "vault_uri" {
 
 output "secret_versionless_id" {
   value = module.keyvault.secret_versionless_id
+}
+
+output "uami_id" {
+  value = module.identity.id
+}
+
+output "uami_principal_id" {
+  value = module.identity.principal_id
 }
