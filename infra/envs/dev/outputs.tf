2e9fa733578e7b8e9c7e1781c1c643c8d87d79ae
Jose Stiven Correa Valoyes <103680054+JoseCorreaV@users.noreply.github.com>
2026-02-12 14:09:43 -0500

    Update outputs.tf

--- infra/envs/dev/outputs.tf
+++ infra/envs/dev/outputs.tf
@@ -6,18 +6,6 @@ output "acr_login_server" {
   value = module.acr.login_server
 }
 
-output "keyvault_id" {
-  value = module.keyvault.key_vault_id
-}
-
-output "vault_uri" {
-  value = module.keyvault.vault_uri
-}
-
-output "keyvault_name" {
-  value = "${var.prefix}kv"
-}
-
 output "cae_environment_id" {
   value = module.cae.environment_id
 }
@@ -30,6 +18,18 @@ output "uami_principal_id" {
   value = module.identity.principal_id
 }
 
+output "keyvault_id" {
+  value = module.keyvault.key_vault_id
+}
+
+output "vault_uri" {
+  value = module.keyvault.vault_uri
+}
+
 output "secret_versionless_id" {
   value = module.keyvault.secret_versionless_id
+}
+
+output "keyvault_name" {
+  value = module.keyvault.key_vault_name
 }
