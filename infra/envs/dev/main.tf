5e9b36743eb70da563d5e4bf8f26246160c3b125
Jose Stiven Correa Valoyes <103680054+JoseCorreaV@users.noreply.github.com>
2026-03-03 22:20:46 -0500

    Update main.tf

--- infra/envs/dev/main.tf
+++ infra/envs/dev/main.tf
@@ -6,8 +6,6 @@ resource "azurerm_resource_group" "this" {
 terraform {
   required_version = ">= 1.5.0"
 
-  backend "azurerm" {}
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
