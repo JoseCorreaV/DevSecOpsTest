3e0725d389a8c24471370f94ba4e713b9d88add6
Jose Stiven Correa Valoyes <103680054+JoseCorreaV@users.noreply.github.com>
2026-03-03 21:44:45 -0500

    Update regex function for container registry name

--- infra/modules/acr/main.tf
+++ infra/modules/acr/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_container_registry" "this" {
-  name                = "${lower(replace(var.prefix, "/[^0-9A-Za-z]/", ""))}acr"
+  name                = "${lower(regexreplace(var.prefix, "[^0-9A-Za-z]", ""))}acr"
   resource_group_name = var.resource_group_name
   location            = var.location
 
